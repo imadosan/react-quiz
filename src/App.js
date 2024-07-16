581168c620cfab4e2672d7d51c11ee3f3a020734
imadosan <imad.fayad.if@gmail.com>
2024-07-16 12:22:23 +0200

    loading questions from our 'fake' api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataRecived', payload: data }))
+      .catch((e) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
