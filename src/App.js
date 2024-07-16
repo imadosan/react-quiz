5a232bb402e16dbd68df622daf52d4e9ab43d02b
imadosan <imad.fayad.if@gmail.com>
2024-07-16 11:53:04 +0200

    added skeleton layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import DateCounter from './DateCounter';
+import Header from './components/Header';
+import Main from './components/Main';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
